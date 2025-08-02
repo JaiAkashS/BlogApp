8f95f7819a05302c8ad4660f3d37d97e9bd197da
jai <jaiakash2907@gmail.com>
2025-08-02 18:15:39 +0530

    changed dependency array in useeffect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,14 +8,14 @@ export default function Home(){
     const [blogs,setBlogs] = useState([])
       useEffect(() => {    console.log('effect')
                 blogsService.getAll().then(initialBlogs =>setBlogs(initialBlogs))           
-            })
+            },[])
     return(
         <>
             <div><Link to="/create">New Post</Link></div>
             <div>
                 {
                     blogs.map(blog=>{
-                     return(                        <BlogThumb key={blog.slug} b={blog} ></BlogThumb>
+                     return(      <BlogThumb key={blog.slug} b={blog} ></BlogThumb>
                     )
                     })
                 }
--- src/services/blogsService.js
+++ src/services/blogsService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/Blogs'
+const baseUrl = 'http://localhost:6969/api/blogs'
 
 const getAll = ()=>{
     let response = axios.get(baseUrl)
